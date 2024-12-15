0e0887f9cfcfe97b6f8f59c4dc952a7a8dda057e
blah148 <info@ploddings.com>
2024-12-15 12:17:35 -0700

    fixed css

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -5,24 +5,26 @@ import styles from '../styles/Sidebar.module.css';
 export default function Sidebar() {
   return (
     <div className={styles.sidebar}>
-      <Link href="/" passHref>
-        <p style={{ cursor: 'pointer', color: 'blue', marginBottom: '30px' }}>Home</p>
-      </Link>
-      <Link href="/bio" passHref>
-        <p style={{ cursor: 'pointer', color: 'blue' }}>Words</p>
-      </Link>
-      <Link href="/music" passHref>
-        <p style={{ cursor: 'pointer', color: 'blue' }}>music</p>
-      </Link>
-      <Link href="/dates" passHref>
-        <p style={{ cursor: 'pointer', color: 'blue' }}>Dates</p>
-      </Link>
-      <Link href="/gallery" passHref>
-        <p style={{ cursor: 'pointer', color: 'blue' }}>gallery</p>
-      </Link>
-      <Link href="/write" passHref>
-        <p style={{ cursor: 'pointer', color: 'blue' }}>Contact</p>
-      </Link>
+			<div className={styles.sticky}>
+				<Link href="/" passHref>
+					<p style={{ cursor: 'pointer', color: 'blue', marginBottom: '30px' }}>Home</p>
+				</Link>
+				<Link href="/bio" passHref>
+					<p style={{ cursor: 'pointer', color: 'blue' }}>Words</p>
+				</Link>
+				<Link href="/music" passHref>
+					<p style={{ cursor: 'pointer', color: 'blue' }}>music</p>
+				</Link>
+				<Link href="/dates" passHref>
+					<p style={{ cursor: 'pointer', color: 'blue' }}>Dates</p>
+				</Link>
+				<Link href="/gallery" passHref>
+					<p style={{ cursor: 'pointer', color: 'blue' }}>gallery</p>
+				</Link>
+				<Link href="/write" passHref>
+					<p style={{ cursor: 'pointer', color: 'blue' }}>Contact</p>
+				</Link>
+			</div>
     </div>
   );
 }
